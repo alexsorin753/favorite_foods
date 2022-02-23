2faa4a8443d3ac49eb3aca122c5100efaf985b1c
alexsorin753 <alex.songureanu74@gmail.com>
2022-02-23 14:10:43 +0200

    ready to go

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var livereload = require('livereload');
+var liveReloadServer = livereload.createServer();
+liveReloadServer.watch(__dirname + "/public");
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => liveReloadServer.refresh("/"), 0);
+});
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// static files
+app.use(express.static('public'));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +28,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET bauturi page.
+router.get('/bauturi', function(req, res, next) {
+  res.render('bauturi', {title: 'Bauturi'});
+});
+
+// GET brutarie-patiserie page.
+router.get('/brutarie-si-patiserie', function(req, res, next) {
+  res.render('brutarie-patiserie', {title: 'Brutarie si Patiserie'});
+});
+
+// GET carse-peste page.
+router.get('/carne-si-peste', function(req, res, next) {
+  res.render('carne-peste', {title: 'Carne si Peste'});
+});
+
+// GET fructe-legume page.
+router.get('/fructe-si-legume', function(req, res, next) {
+  res.render('fructe-legume', {title: 'Fructe si Legume'});
+});
+
+// GET lactate-branzeturi page.
+router.get('/lactate-si-branzeturi', function(req, res, next) {
+  res.render('lactate-branzeturi', {title: 'Lactate si Branzeturi'});
+});
+
+// GET mezeluri-realymeals page.
+router.get('/mezeluri-si-readymeals', function(req, res, next) {
+  res.render('mezeluri-readymeals', {title: 'Mezeluri si Readymeals'});
+});
+
+// GET conserve page.
+router.get('/conserve', function(req, res, next) {
+  res.render('conserve', {title: 'Consrve'});
+});
+
+// GET dulciuri-micdejun page.
+router.get('/dulciuri-si-micdejun', function(req, res, next) {
+  res.render('dulciuri-micdejun', {title: 'Dulciuri si Micdejun'});
 });
 
 module.exports = router;
